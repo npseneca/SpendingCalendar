ce7b4d472f8f454d474de8809de467c0ff909338
npseneca <npseneca@gmail.com>
2024-07-02 21:56:56 -0400

    Initial Commit

---
+++ SpendingCalendarApp/SpendingCalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingCalendarAppApp.swift
+//  SpendingCalendarApp
+//
+//  Created by Noah Seneca on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SpendingCalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
